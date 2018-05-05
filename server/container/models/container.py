e563ef14e806ea6a32501174293d7c4003aec8f4
Linus Basig <linus@basig.me>
2018-05-05 07:04:17 +0200

    Update model field

--- server/container/models/container.py
+++ server/container/models/container.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Container(models.Model):
-    container_id = models.CharField(max_length=100, primary_key=True, editable=False)
-    tracker_id = models.CharField(max_length=100)
+    container_id = models.CharField(max_length=6, primary_key=True, editable=False)
+    tracker_id = models.CharField(max_length=100, unique=True)
