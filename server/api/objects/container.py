8b5d6712601edfb1d58dddee99d1e095aec0749e
Linus Basig <linus@basig.me>
2018-05-05 05:09:19 +0200

    Add container location to api

--- server/api/objects/container.py
+++ server/api/objects/container.py
@@ -2,8 +2,18 @@ import graphene
 from graphene_django.types import DjangoObjectType
 
 from container.models import Container
-
+from location.api_client import get_data
 
 class ContainerType(DjangoObjectType):
     class Meta:
         model = Container
+
+    lon = graphene.Float()
+    lat = graphene.Float()
+
+    def resolve_lon(self, info, **kwargs):
+        return get_data(self.tracker_id)[0]['lon']
+
+    def resolve_lat(self, info, **kwargs):
+        return get_data(self.tracker_id)[0]['lat']
+        
---
+++ server/location/api_client.py
@@ -0,0 +1,13 @@
+import requests
+
+BASE_URL = 'https://containertracker.data.thethingsnetwork.org'
+HEADERS = {
+    'Authorization': 'key ttn-account-v2.dkrPsi4bpOb-vElzq529wjzyitZN_XIehaVDjKr_1xY'
+}
+
+def get_devices():
+    return requests.get( f'{BASE_URL}/api/v2/devices', headers=HEADERS).json()
+
+
+def get_data(tracker_id):
+    return requests.get( f'{BASE_URL}/api/v2/query/{tracker_id}', headers=HEADERS).json()
